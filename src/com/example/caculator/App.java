d0c57f4e188e8995b6820b1e9063ddf5d70e5b8e
parkjj0408 <qhthrrhdwn@naver.com>
2025-04-16 16:11:45 +0900

    계산기과제

---
+++ src/com/example/caculator/App.java
@@ -0,0 +1,45 @@
+package com.example.caculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         while(true) {
+             System.out.println("첫 번째 정수를 입력해 주세요");
+
+             int a = sc.nextInt();
+
+             System.out.println("두 번째 정수를 입력해 주세요");
+             int b = sc.nextInt();
+             if (b == 0) {
+                 System.out.println("0은 입력이 안됩니다");
+                 b = sc.nextInt();
+             }
+             System.out.println("사칙연산 기호를 입력하세요");
+
+             char op = sc.next().charAt(0);
+             if (op == '+') {
+                 System.out.println("결과 값 : " + (a + b));
+             } else if (op == '-') {
+                 System.out.println("결과 값 : " + (a - b));
+             } else if (op == '*') {
+                 System.out.println("결과 값 : " + (a * b));
+             } else if (op == '/') {
+                 System.out.println("결과 값 : " + (a / b));
+             }
+             sc.nextLine();
+             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+             String end = sc.nextLine();
+
+
+             if(end.equals("exit")){
+                 System.out.println("종료되었습니다");
+                 break;
+             }
+
+        }
+
+    }
+    }
+
