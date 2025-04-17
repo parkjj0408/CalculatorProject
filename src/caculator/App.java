101517ff266e1c8b6ee3dc7874449d3fa4b3d3eb
parkjj0408 <qhthrrhdwn@naver.com>
2025-04-17 14:07:09 +0900

    계산기 과제 2단계

--- src/caculator/App.java
+++ src/caculator/App.java
@@ -1,49 +1,46 @@
 package caculator;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
         Scanner sc = new Scanner(System.in);
+
          while(true) {
              System.out.println("첫 번째 정수를 입력해 주세요");
 
-             int a = sc.nextInt();
+             int num1 = sc.nextInt();
 
              System.out.println("두 번째 정수를 입력해 주세요");
-             int b = sc.nextInt();
-             if (b == 0) {
-                 System.out.println("0은 입력이 안됩니다");
-                 b = sc.nextInt();
+             int num2 = sc.nextInt();
+             if (num2 == 0) {
+                 System.out.println("0은 입력할 수 없습니다");
+                 System.out.println("다시 입력해 주세요.");
+                 num2 = sc.nextInt();
              }
+
              System.out.println("사칙연산 기호를 입력하세요");
 
              char op = sc.next().charAt(0);
-             if (op == '+') {
-                 int result = a+b;
-                 System.out.println("결과 값 : " + result);
-             } else if (op == '-') {
-                 int result = a-b;
-                 System.out.println("결과 값 : " + result);
-             } else if (op == '*') {
-                 int result = (a*b);
-                 System.out.println("결과 값 : " + result);
-             } else if (op == '/') {
-                 int result = (a/b);
-                 System.out.println("결과 값 : " + result);
-             }
+
+             int result = calculator.calculate(num1, num2, op);
+             System.out.println("결과 값 :" + result);
+
              sc.nextLine();
-             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+             System.out.println("더 계산하시겠습니까? 아무 글자나 입력해주세요.(exit 입력 시 종료)");
              String end = sc.nextLine();
 
-
              if(end.equals("exit")){
-                 System.out.println("종료되었습니다");
                  break;
              }
 
         }
-
+        System.out.println("값 :" + calculator.getResult());
+        calculator.removeResult();
     }
     }
 
--- src/caculator/Calculator.java
+++ src/caculator/Calculator.java
@@ -1,25 +1,48 @@
 package caculator; // 쓰일 멤버변수 // 사칙연산을 수행하는 메서드
 
+
+import java.util.ArrayList;
+
 public class Calculator {
-    String s;
-    int a;
-    int b;
+  private ArrayList<Integer> results = new ArrayList<Integer>();// 연산결과를 저장하는컬렉션 타입  필드 선언 및 생성
 
-    int add(int a, int b) {//더하기
-        int result = a + b;
-        return result;
-    }
+  public int calculate(int num1, int num2, char op) {
+    switch (op) {
+      case '+':
+        results.add(num1 + num2); //인덱스 안에 넣기 성공 ?
+        return num1 + num2;
 
-    int substract(int a, int b) {
-        int result = a - b;
-        return result;
+      case '-':
+        results.add(num1 - num2);
+        return num1 - num2;
+
+
+      case '*':
+        results.add(num1 * num2);
+        return num1 * num2;
+
+      case '/':
+        if (num2 == 0) {
+          System.out.println("0은 입력하실 수 없습니다.");
+        } else
+          results.add(num1 / num2);
+        return num1 / num2;
+
+      default:
+        throw new IllegalArgumentException("기호 입력이 잘못 되었습니다");
     }
-     int multiply(int a, int b){
-        int result = (a*b);
-        return result;
-     }
-     int divide(int a, int b){
-        int result = (a/b);
-        return result;
-     }
+  }
+
+  public ArrayList<Integer> getResult() {
+    return results;
+  }
+
+  public void removeResult() {
+    int removedValue = results.remove(0); // 삭제된 값을 보여주기 위해 변수에 저장.
+    if (!results.isEmpty()) {
+      System.out.println("삭제된 값 : " + removedValue);
+      results.remove(0);//먼저 입력된 값 삭제 list.remove(인덱스 번호)
+    }
+  }
 }
+
