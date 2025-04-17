9ebbfc9fc09b737b6497fe8efe83fb7ee0646426
parkjj0408 <qhthrrhdwn@naver.com>
2025-04-17 19:05:56 +0900

    계산기 과제 2단계

--- src/caculator/Calculator.java
+++ src/caculator/Calculator.java
@@ -2,46 +2,60 @@ package caculator; // 쓰일 멤버변수 // 사칙연산을 수행하는 메서
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
-  private ArrayList<Integer> results = new ArrayList<Integer>();// 연산결과를 저장하는컬렉션 타입  필드 선언 및 생성
+  private List<Integer> results = new ArrayList<>();// 연산결과를 저장하는컬렉션 타입  필드 선언 및 생성
+
 
   public int calculate(int num1, int num2, char op) {
+    int result;
     switch (op) {
       case '+':
-        results.add(num1 + num2); //인덱스 안에 넣기 성공 ?
-        return num1 + num2;
+        result = num1 + num2;
+        results.add(result);//인덱스 안에 넣기 성공 ?
+        return result ;
 
       case '-':
-        results.add(num1 - num2);
-        return num1 - num2;
+        result = num1 - num2;
+        results.add(result);
+        return result;
 
 
       case '*':
-        results.add(num1 * num2);
-        return num1 * num2;
+        result = num1 * num2;
+        results.add(result);
+        return result ;
 
       case '/':
-        if (num2 == 0) {
-          System.out.println("0은 입력하실 수 없습니다.");
-        } else
-          results.add(num1 / num2);
-        return num1 / num2;
+        if (num2!= 0) {
+        result = num1 / num2;
+        results.add(result);
+        return result ;
 
-      default:
-        throw new IllegalArgumentException("기호 입력이 잘못 되었습니다");
+        } else {
+          System.out.println("0은 입력하실 수 없습니다.");
+
+        }
+      default  :
+        throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
     }
+
   }
 
-  public ArrayList<Integer> getResult() {
+  public List<Integer> getResult() {
     return results;
   }
+  //public void clearResult(){ 값 초기화 메서드
+  // results.clear();
+  // }
 
   public void removeResult() {
-    int removedValue = results.remove(0); // 삭제된 값을 보여주기 위해 변수에 저장.
+    // 삭제된 값을 보여주기 위해 변수에 저장.
     if (!results.isEmpty()) {
+      int removedValue = results.remove(0);
       System.out.println("삭제된 값 : " + removedValue);
-      results.remove(0);//먼저 입력된 값 삭제 list.remove(인덱스 번호)
+      //먼저 입력된 값 삭제 list.remove(인덱스 번호)
     }
   }
 }
